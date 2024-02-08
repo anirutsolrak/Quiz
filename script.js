4cb4a7ed8d94a317bfa24d184b155dbb8e13ca37
anirutsolrak <146773453+anirutsolrak@users.noreply.github.com>
2024-02-08 09:43:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -89,21 +89,52 @@ const perguntas = [
     ],
     correta: 1
   }
-]
-const quiz = document.querySelector("#quiz")
+];
+const quiz = document.querySelector('#quiz')
 const template = document.querySelector('template')
 
-for(const item of perguntas){
-const quizItem = template.content.cloneNode(true)
-quizItem.querySelector("h3").textContent = item.pergunta
+const corretas = new Set()
+const totalDePerguntas = perguntas.length
+const mostrarTotal = document.querySelector('#acertos span')
+mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
 
-for(let resposta of item.respostas){
-  const dt = quizItem.querySelector('dl dt').cloneNode(true)
-  dt.querySelector('span').textContent = resposta
+// loop ou laço de repetição
+for (const item of perguntas) {
+  const quizItem = template.content.cloneNode(true)
+  quizItem.querySelector('h3').textContent = item.pergunta
 
-  quizItem.querySelector('dl').appendChild(dt)
+  for (let resposta of item.respostas) {
+    const dt = quizItem.querySelector('dl dt').cloneNode(true)
+    dt.querySelector('span').textContent = resposta
+    dt.querySelector('input').setAttribute('name', 'pergunta-' + perguntas.indexOf(item))
+    dt.querySelector('input').value = item.respostas.indexOf(resposta)
+    dt.querySelector('input').onchange = (event) => {
+      const estaCorreta = event.target.value == item.correta
+
+      corretas.delete(item)
+      if (estaCorreta) {
+        corretas.add(item)
+      }
+
+      mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
+    }
+    quizItem.querySelector('dl').appendChild(dt)
+  }
+
+
+  quizItem.querySelector('dl dt').remove()
+
+
+  // coloca a pergunta na tela
+  quiz.appendChild(quizItem)
 }
 
-quizItem.querySelector('dl dt').remove()
-quiz.appendChild(quizItem)
-}
+
+
+
+
+
+
+
+
+
